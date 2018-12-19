44edf813866de4208564cf4dcaa4235856cda6e0
Miridinia <carolm@sapo.pt>
2018-12-19 14:29:19 +0000

    Circle done :D

--- squares.pl
+++ squares.pl
@@ -20,21 +20,22 @@ test:-
 
 row_sums(Sums):- Sums = [23, 20, 26, 20, 26, 19, 22]. 
 
-twod_same_size(Board, Copy):-
-    length(Board, Lenght),
-    length(Copy, Lenght),
-    oned_same_size(Board, Copy).
+twod_same_size(Board, Copy, NumRows, NumCols):-
+    length(Board, NumRows),
+    length(Copy, NumRows),
+    oned_same_size(Board, Copy, NumCols).
 
-oned_same_size([],[]).
-oned_same_size([H|RemainingBoard], [H2|RemainingCopy]) :-
-    length(H, Lenght),
-    length(H2, Lenght),
-    oned_same_size(RemainingBoard, RemainingCopy).    
+oned_same_size([],[], _).
+oned_same_size([H|RemainingBoard], [H2|RemainingCopy], NumCols) :-
+    length(H, NumCols),
+    length(H2, NumCols),
+    oned_same_size(RemainingBoard, RemainingCopy, NumCols).    
 
 solve(Board, Sums, Solution) :-
-    twod_same_size(Board, Solution),
+    twod_same_size(Board, Solution, NumRows, NumCols),   
     restrict_domain(Solution),
     restrict_row_sums(Sums, Solution),
+    restrict_circle(Board, Solution),
     flatten(Solution, FlatVariables),
     labeling([], FlatVariables).
 
@@ -52,3 +53,35 @@ restrict_row_sums([], []).
 restrict_row_sums([RowSum|Sums],[Row|Solution]):-
     sum(Row, #=, RowSum),
     restrict_row_sums(Sums, Solution).
+
+restrict_circle(Board, Solution):-
+    get_all_circles(Board, Solution, [H|CircleVars]),
+    not_multiple_three(H),
+    all_equal([H|CircleVars]).
+
+all_equal([_]).
+all_equal([]).
+all_equal([H1|[H2|T]]) :-
+    H1 #= H2,
+    all_equal([H2|T]).
+
+not_multiple_three(Num):-
+    Num rem 3 #\= 0.
+
+get_all_circles([], [], []).
+get_all_circles([Row|Board], [SolRow|Solution], CircleVars):-
+    get_circles_row(Row, SolRow, CircleRow),
+    get_all_circles(Board, Solution, OtherCircles),
+    append(CircleRow, OtherCircles, CircleVars).
+
+get_circles_row([], [], []).
+get_circles_row([circle|Row], [CircleVar|SolRow], [CircleVar|CircleRow]):-
+    get_circles_row(Row, SolRow, CircleRow).
+
+get_circles_row([NotCircle|Row], [_|SolRow], CircleRow):-
+    NotCircle \= circle,
+    get_circles_row(Row, SolRow, CircleRow).
+
+
+
+
